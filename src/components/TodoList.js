78bf80998e354859802ca1e5e74364a964850066
Neeraj <shukla7071@gmail.com>
2026-01-05 18:25:26 +0530

    try24

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,48 @@
-import React from "react";
-
+import React, { useState } from "react";
 const TodoList = ({ todos, handleComplete }) => {
   return (
     <div>
       <h2>Child Component</h2>
-
       <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.text}  
-            {!todo.completed ? (
-              <button onClick={() => handleComplete(todo.id)}>
-                Complete
-              </button>
-            ) : (
-              <span>Complete</span>
-            )}
-          </li>
-        ))}
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}&nbsp;&nbsp;
+          {!todo.isCompleted && (
+            <button onClick={() => handleComplete(todo.id)}>Complete</button>
+          )}
+        </li>
+      ))}
       </ul>
     </div>
   );
 };
 
 export default TodoList;
+
+
+// import React from "react";
+
+// const TodoList = ({ todos, handleComplete }) => {
+//   return (
+//     <div>
+//       <h2>Child Component</h2>
+
+//       <ul>
+//         {todos.map(todo => (
+//           <li key={todo.id}>
+//             {todo.text}  
+//             {!todo.completed ? (
+//               <button onClick={() => handleComplete(todo.id)}>
+//                 Complete
+//               </button>
+//             ) : (
+//               <span>Complete</span>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default TodoList;
