a121014fedb3c62843653d235aba89d55f5f3580
Neeraj <shukla7071@gmail.com>
2026-01-05 18:32:58 +0530

    try25

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,48 +1,44 @@
-import React, { useState } from "react";
-const TodoList = ({ todos, handleComplete }) => {
-  return (
-    <div>
-      <h2>Child Component</h2>
-      <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          {todo.text}&nbsp;&nbsp;
-          {!todo.isCompleted && (
-            <button onClick={() => handleComplete(todo.id)}>Complete</button>
-          )}
-        </li>
-      ))}
-      </ul>
-    </div>
-  );
-};
-
-export default TodoList;
-
-
-// import React from "react";
-
+// import React, { useState } from "react";
 // const TodoList = ({ todos, handleComplete }) => {
 //   return (
-//     <div>
+//     <ul>
 //       <h2>Child Component</h2>
-
-//       <ul>
-//         {todos.map(todo => (
-//           <li key={todo.id}>
-//             {todo.text}  
-//             {!todo.completed ? (
-//               <button onClick={() => handleComplete(todo.id)}>
-//                 Complete
-//               </button>
-//             ) : (
-//               <span>Complete</span>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
+//       {todos.map((todo) => (
+//         <li key={todo.id}>
+//           {todo.text}&nbsp;&nbsp;
+//           {!todo.isCompleted && (
+//             <button onClick={() => handleComplete(todo.id)}>Complete</button>
+//           )}
+//         </li>
+//       ))}
+//     </ul>
 //   );
 // };
 
 // export default TodoList;
+
+
+import React from "react";
+
+const TodoList = ({ todos, handleComplete }) => {
+  return (
+      <ul>
+      <h2>Child Component</h2>
+
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}  
+            {!todo.isCompletd? (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            ) : (
+              <span>Complete</span>
+            )}
+          </li>
+        ))}
+      </ul>
+  );
+};
+
+export default TodoList;
