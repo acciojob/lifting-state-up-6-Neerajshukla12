13adc7950d6a308c7ee46557e4e26bb713b889e6
Neeraj <shukla7071@gmail.com>
2026-01-05 20:12:59 +0530

    try26

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -24,10 +24,9 @@ const TodoList = ({ todos, handleComplete }) => {
   return (
       <ul>
       <h2>Child Component</h2>
-
         {todos.map(todo => (
           <li key={todo.id}>
-            {todo.text}&nbsp;&nbsp;
+            {todo.text}
             {!todo.isCompleted &&(
               <button onClick={() => handleComplete(todo.id)}>Complete</button>)
             }
