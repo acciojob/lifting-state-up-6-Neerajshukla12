b63d2fbfe2cf21b42828a50cce7e237a8d1b964c
Neeraj <shukla7071@gmail.com>
2026-01-05 18:20:07 +0530

    try23

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+
 const TodoList = ({ todos, handleComplete }) => {
   return (
-    <ul>
+    <div>
       <h2>Child Component</h2>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          {todo.text}&nbsp;&nbsp;
-          {!todo.isCompleted && (
-            <button onClick={() => handleComplete(todo.id)}>Complete</button>
-          )}
-        </li>
-      ))}
-    </ul>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}  
+            {!todo.completed ? (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            ) : (
+              <span>Complete</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
